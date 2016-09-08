02c177728435f6a64ccb0ab99148b146785fc5dd
Travis <Travis@10.0.0.169>
2016-09-07 20:26:47 -0700

    working on vortex sheet induction function

--- Misc/VAP Induction Test - Semi-Infinite/Comparison.m
+++ Misc/VAP Induction Test - Semi-Infinite/Comparison.m
@@ -1,25 +1,70 @@
-% clc
+clc
 clear
 
 endpoints(:,:,1) = [0 0 0];
 endpoints(:,:,2) = [0 1 0];
 
-fpg = [3 2 2];
+fpg = [0 0 0; 1 2 5; 0.5 0.5 0; 0.5 1 0];
+phi = [0 0 0 0]';
+
+
+endpoints = repmat(endpoints,4,1,1);
 
 orig = mean(endpoints,3);
 
-k = 0;
+k = [10 10 10 10]';
+eta_VS = [0 1 0; 0 1 0; 0 1 0; 0 1 0];
+% eta_VS = [1 0 0; 1 0 0; 1 0 0; 1 0 0];
 
-%%
+[al, bl, cl] = fcnVSIND(endpoints, phi, eta_VS, fpg, k);
+
 Temp.DBL_EPS = 1e-14;
 nuj = 0;
 phij = 0;
 etaj = 0.5;
 
-[a2x, b2x, c2x] = fcnVortexSheetInduction(Temp, fpg, orig, nuj, phij, etaj, k)
+for i = 1:4
+[a2x(i,:), b2x(i,:), c2x(i,:)] = fcnVortexSheetInduction(Temp, fpg(i,:), orig(i,:), nuj, phij, etaj, k(i));
+end
+
+al
+a2x
+
+bl
+b2x
+
+cl
+c2x
 
 %%
 
-% assuming global ref frame is equal to local 
+% clc
+clear
 
-[al, bl, cl] = fcnVSIND(endpoints, 0, [0 1 0], fpg, k)
+endpoints(:,:,1) = [0 0 0];
+endpoints(:,:,2) = [1 0 0];
+
+fpg = [0 0 0; 1 2 5; 0.5 0.5 0; 0.5 1 0];
+phi = [0 0 0 0]';
+
+
+endpoints = repmat(endpoints,4,1,1);
+
+orig = mean(endpoints,3);
+
+k = [10 10 10 10]';
+% eta_VS = [0 1 0; 0 1 0; 0 1 0; 0 1 0];
+eta_VS = [1 0 0; 1 0 0; 1 0 0; 1 0 0];
+
+Temp.DBL_EPS = 1e-14;
+nuj = 0;
+phij = 0;
+etaj = 0.5;
+
+% assuming global ref frame is equal to local 
+num = 2;
+[a2x, b2x, c2x] = fcnVortexSheetInduction(Temp, fpg(num,:), orig(num,:), nuj, phij, etaj, k(num));
+[al, bl, cl] = fcnVSIND(endpoints(num,:,:), phi(num), eta_VS(num,:), fpg(num,:), k(num));
+
+
+

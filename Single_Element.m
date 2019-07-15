d037eee0462b83c2c859e942119e4ee4de81f5bf
travi <tkrebs@ryerson.ca>
2019-07-15 11:43:18 -0400

    Updating on surface corrections to induction model (as per FTJ)

--- Single_Element.m
+++ Single_Element.m
@@ -20,9 +20,9 @@ vecSYM = [];
 matPOINTS(:,:,1) = [1 0 0];
 matPOINTS(:,:,2) = [0  0 0];
 % matPOINTS(:,:,3) = [0.9  0.5 0];
-xp = 0
+% xp = 0
 % xp = 0.99
-% xp = 0.5
+xp = 0.5
 
 matPOINTS(:,:,3) = [xp  -0.5 0];
 
@@ -51,36 +51,20 @@ matCOEFF = -[0 0 -2 0.17 0 0.056];
 % y = [-0.1:granularity:0.6];
 % z = [-0.1:granularity:0.1];
 
-% % % granularity = 0.00125;
-% granularity = 0.04
-% x = [-0.1:granularity:0.9];
-% y = [-0.1:granularity:0.6];
-% % y = [0.01:granularity:0.49];
-% z = [-0.15 -0.05 0 0.05 0.15];
-% % z = [1 -1];
+% granularity = 0.005
+% x = [-0.5:granularity:1.5];
+% y = [-0.8:granularity:1.3];
+% z = [-1:granularity:1];
+% % z = [-0.5 0.5];
 % z = 0;
-
-% granularity = 0.025
-% x = [-0.1:granularity:1];
-% y = [-0.2:0.05:0.7];
-% z = [-0.05 0 0.05];
-% % z = [-0.05 0.05];
-% % z = 0;
-
-granularity = 0.05
-x = [-0.5:granularity:1.5];
-y = [-0.8:granularity:1.3];
-z = [-1:granularity:1];
-% z = [-0.5 0.5];
-z = 0;
 
 % granularity = 0.25
 % x = [-1:granularity:2];
 % y = [-1:granularity:2];
 % z = [-2:granularity:2];
-% % z = [1e-1 -1e-1];
-% % z = 0;
-% % z(z == 0) = []
+% z = [1e-1 -1e-1];
+% z = 0;
+% z(z == 0) = []
 
 % granularity = 0.5
 % x = [-3:granularity:5];
@@ -88,54 +72,23 @@ z = 0;
 % z = [-4:granularity:5];
 % z(z == 0) = []
 
-% granularity = 0.00125;
-% granularity = 0.1
-% x = [-0.5:granularity:1.5];
-% y = [-0.5:granularity:1];
-% z = [-0.3:granularity:0.3];
-
-% % granularity = 0.00125;
-% granularity = 0.05
-% x = [0.05:granularity:0.95];
-% % x = 0
-% % y = [-1:granularity:1];
-% y = [0.05:granularity:0.55];
-% % z = [-1:granularity:1];
-% z = [1e-5 -1e-5];
-% z(z == 0) = []
+granularity = 0.00005;
+x = 0.5;
+y = 0.15;
+z = [-0.01:granularity:0.01];
 
 [X,Y,Z] = meshgrid(x,y,z);
 fpg = unique([reshape(X,[],1) reshape(Y,[],1) reshape(Z,[],1)],'rows');
 
-% num = 100;
-% fpg = [linspace(0,xp,num)' linspace(0,0.5,num)' zeros(num,1)];
-
-% fpg = [0.5 0 0; 0.6 0 0; 0.5 0.001 0; 0.6 0.001 0]
-% fpg = [0.5 0.2 0; 0.5 0.2 0];
-
-% fpg = [0.3 0.5 0.0; 0.3 0.5 0.0]
-% fpg = [0.1 0.1 0.0; 0.1 0.1 0.0]
-% fpg = [0.7 0.2 0.0; 0.7 0.2 0.0]
-% fpg = [0 1.8 0.8; 0 1.8 0.8]
-% fpg = [-0.1 1.3 -0.1; -0.1 1.3 -0.1]
-% fpg = [-2 -1 -0.1; -2 -1 -0.1]
-% fpg = repmat([1 0 0], 2, 1)
-
-% x1 = (0.2 - 0.5)./(((-0.5)/(1 - xp)));
-% fpg = [0 0.2 0; x1 0.2 0];
-% vecBOUNDIND = true(size(fpg,1),1);
-% vecBOUNDIND = false(size(fpg,1),1);
-% fpg = [fpg];
-
-% fpg = [-0.5 0.75 0; -0.5 0.625 0];
-% fpg = matCENTER
+% fpg = [0.5 0.15 0.0001; 0.1 0.05 0.0001]
 [q_ind] = fcnSDVEVEL(fpg, valNELE, matCOEFF, matPLEX, matROTANG, matCONTROL, vecDVESYM, [], 0);
+
+points = fcnGLOBSTAR([0.5 0.15 0] - matCENTER, matROTANG);
+vort = [matCOEFF(1,3).*points(:,1) + matCOEFF(1,4), matCOEFF(1,1).*points(:,2) + matCOEFF(1,2), points(:,2).*0];
 
 % vecBOUNDIND = true(size(fpg,1),1);
 % [q_ind2] = fcnSDVEVEL(fpg, valNELE, matCOEFF, matPLEX, matROTANG, matCONTROL, vecDVESYM, vecBOUNDIND);
-% q_ind
-% 
-% q_ind2 = q_ind + q_ind2;
+
 %%
 % figure(1);
 % clf(1);
@@ -144,12 +97,12 @@ quiver3(fpg(:,1), fpg(:,2), fpg(:,3), q_ind(:,1), q_ind(:,2), q_ind(:,3), 'b')
 % quiver3(fpg(:,1), fpg(:,2), fpg(:,3), q_ind2(:,1), q_ind2(:,2), q_ind2(:,3), 1, 'm')
 hold off
 
-% % figure(2);
-% % clf(2);
-% plot(z, q_ind(:,3),'-ok')
-% grid minor
-% box on
-% axis tight
+figure(2);
+clf(2);
+plot(q_ind(:,2), z, '-ok')
+grid minor
+box on
+axis tight
 
 
 
--- fcnHDVEIND_DB.m
+++ fcnHDVEIND_DB.m
@@ -1,6 +1,7 @@
 function [infl_loc] = fcnHDVEIND_DB(dvenum, dvetype, fpg, matPLEX, matROTANG, matCONTROL, vecBI, ztol)
 warning('on')
 tol = 1e-2;
+k = 0;
 
 del_h = 0.01;
 
@@ -39,7 +40,7 @@ xi_left = min([xi_1, xi_3],[],2);
 xi_right = max([xi_1, xi_3],[],2);
 
 margin = 1e-3;
-idx_on_element = y_m >= te_eta - margin & y_m <= le_eta + margin & x_m >= xi_left - margin & x_m <= xi_right + margin & abs(h) <= margin;
+idx_on_element = y_m >= te_eta - margin & y_m <= le_eta + margin & x_m >= xi_left - margin & x_m <= xi_right + margin;
 
 idx_flp = xi_3 < xi_1; % Flipping influence of elements that need a good flippin
 if any(abs(xi_2 - xi_3) < margin & abs(xi_1 - xi_2) > margin)
@@ -47,22 +48,6 @@ if any(abs(xi_2 - xi_3) < margin & abs(xi_1 - xi_2) > margin)
 end
 
 %%
-% hFig1 = figure(1);
-% clf(1);
-% patch([xi_1(1) xi_2(1) xi_3(1)], [eta_1(1) eta_2(1) eta_3(1)],'w');
-% grid minor
-% box on
-% axis tight
-% axis equal
-% xlabel('Xi-Dir');
-% ylabel('Eta-Dir');
-% zlabel('Zi-Dir');
-% for i = 1:size(fpl,1)
-%     hold on
-%     scatter3(fpl(i,1), fpl(i,2), fpl(i,3), 'rs')
-%     hold off
-% end
-
 % Call, xi-eta, edge (1, 2, or 3), point (1 or 2)
 epts = nan(len,2,3,2);
 epts(:,:,1,1:2) = cat(4, [xi_1 eta_1], [xi_2 eta_2]);
@@ -81,7 +66,15 @@ L1 = dot(nu_d, fpl(:,1:2) - epts(:,:,1:3,1), 2);
 L2 = dot(nu_d, fpl(:,1:2) - epts(:,:,1:3,2), 2);
 a = dot(nu_n, mean(epts,4) - fpl(:,1:2), 2);
 
-d_H = min(mean([sqrt(L1.^2 + a.^2) sqrt(L2.^2 + a.^2)],2),[],3);
+tmp = nan(len,1,3);
+idx = sign(L1) ~= sign(L2);
+tmp(idx) = a(idx);
+idx = (sign(L1) == sign(L2)) & ~idx;
+tmp(idx) = min([L1(idx) L2(idx)],[],2);
+if any(isnan(tmp),'all')
+    disp('Issue in determining d_H');
+end
+d_H = min(abs(tmp),[],3);
 
 g = sqrt(a.^2 + h.^2);
 c1 = g.^2 + abs(h).*sqrt(L1.^2 + g.^2);
@@ -118,21 +111,6 @@ E2129 = (x2 + x1).*E2127 - (x1.*x2.*E2125);
 E2131 = (x2 + x1).*E2129 - (x1.*x2.*E2127);
 E2133 = (x2 + x1).*E2131 - (x1.*x2.*E2129);
 
-% I = 3;
-% E215 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 4;
-% E217 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 5;
-% E219 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 6;
-% E2111 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 7;
-% E2113 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 8;
-% E2115 = A2.*(x2.^(I - 1)) - A1.*(x1.^(I - 1)); 
-% I = 9;
-% E2117 = (x2 + x1).*E2115 - (x1.*x2.*E2113);
-
 % b.)
 A2 = (epts(:,2,:,2) - y_m)./rho2;
 A1 = (epts(:,2,:,1) - y_m)./rho1;
@@ -157,14 +135,6 @@ E1233 = (x2 + x1).*E1231 - (x1.*x2.*E1229);
 
 % c.)
 E11n1 = rho2 - rho1; % I = 1
-E12n1 = rho2.*(epts(:,2,:,2) - y_m) - rho1.*(epts(:,2,:,1) - y_m); % I = 2
-E13n1 = rho2.*(epts(:,2,:,2) - y_m).^2 - rho1.*(epts(:,2,:,1) - y_m).^2; % I = 3
-E14n1 = rho2.*(epts(:,2,:,2) - y_m).^3 - rho1.*(epts(:,2,:,1) - y_m).^3; % I = 4
-
-% d.)
-E21n1 = rho2.*(epts(:,1,:,2) - x_m) - rho1.*(epts(:,1,:,1) - x_m); % I = 2
-E31n1 = rho2.*(epts(:,1,:,2) - x_m).^2 - rho1.*(epts(:,1,:,1) - x_m).^2; % I = 3
-E41n1 = rho2.*(epts(:,1,:,2) - x_m).^3 - rho1.*(epts(:,1,:,1) - x_m).^3; % I = 4
 
 %e.)
 E111 = (1./rho2) - (1./rho1);
@@ -176,16 +146,17 @@ E111 = (1./rho2) - (1./rho1);
 F111 = zeros(size(a));
 
 idx = L1 >= 0 & L2 >= 0;
-F111(idx) = log((rho2(idx) + L2(idx))./(rho1(idx) + L1(idx)));
+F111(idx) = log((rho2(idx) + L2(idx) + k)./(rho1(idx) + L1(idx) + k));
 
 idx = L1 < 0 & L2 < 0;
 F111(idx) = log((rho1(idx) - L1(idx))./(rho2(idx) - L2(idx)));
 
 % idx = L1 < 0 & L2 >= 0;
 idx = (L1 < 0 & L2 >= 0) | (L1 >=0 & L2 < 0);
-F111(idx) = log(((rho1(idx) - L1(idx)).*(rho2(idx) + L2(idx)))./(g(idx).^2));
+F111(idx) = log(((rho1(idx) - L1(idx)).*(rho2(idx) + L2(idx)) + k)./(g(idx).^2) + k);
 
 idx = [g >= del_h.*d_H, g < del_h.*d_H];
+
 % 2.)
 nu_xi = nu_n(:,1,:);
 nu_eta = nu_n(:,2,:);
@@ -268,44 +239,14 @@ F113(idx(:,2,:)) = (1./(K - 3)).*((g(idx(:,2,:)).^2).*(K - 2).*F115(idx(:,2,:)) 
 % (i)
 idx = abs(nu_eta) <= abs(nu_xi);
 F121 = zeros(size(F111));
-F131 = zeros(size(F111));
-F141 = zeros(size(F111));
-F151 = zeros(size(F111));
 F211 = zeros(size(F111));
-F221 = zeros(size(F111));
-F231 = zeros(size(F111));
-F241 = zeros(size(F111));
-F311 = zeros(size(F111));
-F321 = zeros(size(F111));
-F331 = zeros(size(F111));
-F411 = zeros(size(F111));
-F421 = zeros(size(F111));
-F511 = zeros(size(F111));
 
 h = repmat(h,1,1,3);
 F121(idx) = (a(idx).*nu_eta(idx).*F111(idx) + nu_xi(idx).*E11n1(idx));
-N = 3;
-F131(idx) = (1./(N - 1)).*((2.*N - 3).*a(idx).*nu_eta(idx).*F121(idx) - (a(idx).^2 + (nu_xi(idx).^2).*(h(idx).^2)).*F111(idx) + nu_xi(idx).*E12n1(idx));
-N = 4;
-F141(idx) = (1./(N - 1)).*((2.*N - 3).*a(idx).*nu_eta(idx).*F131(idx) - (a(idx).^2 + (nu_xi(idx).^2).*(h(idx).^2)).*F121(idx) + nu_xi(idx).*E13n1(idx));
-N = 5;
-F151(idx) = (1./(N - 1)).*((2.*N - 3).*a(idx).*nu_eta(idx).*F141(idx) - (a(idx).^2 + (nu_xi(idx).^2).*(h(idx).^2)).*F131(idx) + nu_xi(idx).*E14n1(idx));
 
 % PAGE 131
 % (ii)
 F211(idx) = (-nu_eta(idx)./nu_xi(idx)).*F121(idx) + (a(idx)./nu_xi(idx)).*F111(idx);
-F221(idx) = (-nu_eta(idx)./nu_xi(idx)).*F131(idx) + (a(idx)./nu_xi(idx)).*F121(idx);
-F231(idx) = (-nu_eta(idx)./nu_xi(idx)).*F141(idx) + (a(idx)./nu_xi(idx)).*F131(idx);
-F241(idx) = (-nu_eta(idx)./nu_xi(idx)).*F151(idx) + (a(idx)./nu_xi(idx)).*F141(idx);
-
-F311(idx) = (-nu_eta(idx)./nu_xi(idx)).*F221(idx) + (a(idx)./nu_xi(idx)).*F211(idx);
-F321(idx) = (-nu_eta(idx)./nu_xi(idx)).*F231(idx) + (a(idx)./nu_xi(idx)).*F221(idx);
-F331(idx) = (-nu_eta(idx)./nu_xi(idx)).*F241(idx) + (a(idx)./nu_xi(idx)).*F231(idx);
-
-F411(idx) = (-nu_eta(idx)./nu_xi(idx)).*F321(idx) + (a(idx)./nu_xi(idx)).*F311(idx);
-F421(idx) = (-nu_eta(idx)./nu_xi(idx)).*F331(idx) + (a(idx)./nu_xi(idx)).*F321(idx);
-
-F511(idx) = (-nu_eta(idx)./nu_xi(idx)).*F421(idx) + (a(idx)./nu_xi(idx)).*F411(idx);
 
 % b)
 % (i)
@@ -313,27 +254,9 @@ idx = abs(nu_xi) <= abs(nu_eta);
 
 M = 2;
 F211(idx) = (1./(M - 1)).*((2.*M - 3).*a(idx).*nu_xi(idx).*F111(idx) - nu_eta(idx).*E11n1(idx));
-M = 3;
-F311(idx) = (1./(M - 1)).*((2.*M - 3).*a(idx).*nu_xi(idx).*F211(idx) - (M - 2).*(a(idx).^2 + (nu_eta(idx).^2).*(h(idx).^2)).*F111(idx) - nu_eta(idx).*E21n1(idx));
-M = 4;
-F411(idx) = (1./(M - 1)).*((2.*M - 3).*a(idx).*nu_xi(idx).*F311(idx) - (M - 2).*(a(idx).^2 + (nu_eta(idx).^2).*(h(idx).^2)).*F211(idx) - nu_eta(idx).*E31n1(idx));
-M = 5;
-F511(idx) = (1./(M - 1)).*((2.*M - 3).*a(idx).*nu_xi(idx).*F411(idx) - (M - 2).*(a(idx).^2 + (nu_eta(idx).^2).*(h(idx).^2)).*F311(idx) - nu_eta(idx).*E41n1(idx));
 
 % (ii)
 F121(idx) = (-nu_xi(idx)./nu_eta(idx)).*F211(idx) + (a(idx)./nu_eta(idx)).*F111(idx);
-F221(idx) = (-nu_xi(idx)./nu_eta(idx)).*F311(idx) + (a(idx)./nu_eta(idx)).*F211(idx);
-F321(idx) = (-nu_xi(idx)./nu_eta(idx)).*F411(idx) + (a(idx)./nu_eta(idx)).*F311(idx);
-F421(idx) = (-nu_xi(idx)./nu_eta(idx)).*F511(idx) + (a(idx)./nu_eta(idx)).*F411(idx);
-
-F131(idx) = (-nu_xi(idx)./nu_eta(idx)).*F221(idx) + (a(idx)./nu_eta(idx)).*F121(idx);
-F231(idx) = (-nu_xi(idx)./nu_eta(idx)).*F321(idx) + (a(idx)./nu_eta(idx)).*F221(idx);
-F331(idx) = (-nu_xi(idx)./nu_eta(idx)).*F421(idx) + (a(idx)./nu_eta(idx)).*F321(idx);
-
-F141(idx) = (-nu_xi(idx)./nu_eta(idx)).*F231(idx) + (a(idx)./nu_eta(idx)).*F131(idx);
-F241(idx) = (-nu_xi(idx)./nu_eta(idx)).*F331(idx) + (a(idx)./nu_eta(idx)).*F231(idx);
-
-F151(idx) = (-nu_xi(idx)./nu_eta(idx)).*F241(idx) + (a(idx)./nu_eta(idx)).*F141(idx);
 
 % PAGE 132
 % 4.)
@@ -341,8 +264,6 @@ F123 = nu_eta.*a.*F113 - nu_xi.*E111;
 
 % 5.)
 F133 = 2.*a.*nu_eta.*F123 - (a.^2 + (nu_xi.^2).*(h.^2)).*F113 + (nu_xi.^2).*F111;
-F143 = 2.*a.*nu_eta.*F133 - (a.^2 + (nu_xi.^2).*(h.^2)).*F123 + (nu_xi.^2).*F121;
-F153 = 2.*a.*nu_eta.*F143 - (a.^2 + (nu_xi.^2).*(h.^2)).*F133 + (nu_xi.^2).*F131;
 
 % Correct F for proximity to perimeter
 
@@ -357,7 +278,6 @@ H111 = -abs(h).*sum(atan2(a.*(L2.*c1 - L1.*c2), c1.*c2 + (a.^2).*L1.*L2),3) + su
 
 % PAGE 125
 % 2.)
-% idx = [abs(h) >= del_h.*d_H, abs(h) < del_h.*d_H & ~idx_on_element abs(h) < del_h.*d_H & idx_on_element]; 
 idx = [abs(h) >= del_h.*d_H, abs(h) < del_h.*d_H]; 
 
 H113(idx(:,1),1) = (1./h(idx(:,1)).^2).*(-1.*H111(idx(:,1)) + sum(a(idx(:,1),:,:).*F111(idx(:,1),:,:), 3));
@@ -366,6 +286,7 @@ H115(idx(:,1),1) = (1./(3.*h(idx(:,1)).^2)).*(H113(idx(:,1)) + sum(a(idx(:,1),:,
 % PAGE 126
 % 1.)
 H1121 = zeros(size(H111));
+
 % 2.)
 K = 21;
 H1119(idx(:,2),1) = (1./(K - 4)).*((h(idx(:,2)).^2).*(K - 2).*H1121(idx(:,2)) - sum(a(idx(:,2),:,:).*F1119(idx(:,2),:,:), 3)); 
@@ -388,35 +309,12 @@ H113(idx(:,2),1) = (1./(K - 4)).*((h(idx(:,2)).^2).*(K - 2).*H115(idx(:,2)) - su
 K = 3;
 H111(idx(:,2),1) = (1./(K - 4)).*((h(idx(:,2)).^2).*(K - 2).*H113(idx(:,2)) - sum(a(idx(:,2),:,:).*F111(idx(:,2),:,:), 3));
 
-% PAGE 125
-% 3.)
-H211 = (1/2).*((h.^2).*sum(nu_xi.*F111,3) + sum(a.*F211,3));
-H221 = (1/3).*((h.^2).*sum(nu_xi.*F121,3) + sum(a.*F221,3));
-H231 = (1/3).*((h.^2).*sum(nu_xi.*F131,3) + sum(a.*F231,3));
-H241 = (1/3).*((h.^2).*sum(nu_xi.*F141,3) + sum(a.*F241,3));
-
-% 4.)
-H121 = (1/2).*((h.^2).*sum(nu_eta.*F111,3) + sum(a.*F121,3));
-H131 = (1/3).*(-(h.^2).*H111 + (h.^2).*sum(nu_eta.*F121,3) + sum(a.*F131,3));
-H141 = (1/4).*(-(h.^2).*2.*H121 + (h.^2).*sum(nu_eta.*F131,3) + sum(a.*F141,3));
-H151 = (1/5).*(-(h.^2).*2.*H131 + (h.^2).*sum(nu_eta.*F141,3) + sum(a.*F151,3));
-
-% 5.)
-M = 3; N = 1;
-H311 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H111 + (h.^2).*sum(nu_xi.*F211,3) + sum(a.*F311,3));
-M = 4; N = 1;
-H411 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H211 + (h.^2).*sum(nu_xi.*F311,3) + sum(a.*F411,3));
-M = 5; N = 1;
-H511 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H311 + (h.^2).*sum(nu_xi.*F411,3) + sum(a.*F511,3));
-
-M = 3; N = 2;
-H321 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H121 + (h.^2).*sum(nu_xi.*F221,3) + sum(a.*F321,3));
-M = 4; N = 2;
-H421 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H221 + (h.^2).*sum(nu_xi.*F321,3) + sum(a.*F421,3));
-
-M = 3; N = 3;
-H331 = (1./(M + N - 1)).*(-(h.^2).*(M - 2).*H131 + (h.^2).*sum(nu_xi.*F231,3) + sum(a.*F331,3));
-
+% PAGE 127
+idx = idx(:,2) & idx_on_element;
+% idx = [];
+H111(idx) = H111(idx) - eps(1,1,1,h(idx));
+H113(idx) = H113(idx) - eps(1,1,3,h(idx));
+H115(idx) = H115(idx) - eps(1,1,5,h(idx));
 
 % PAGE 126
 % 6.)
@@ -424,10 +322,6 @@ K = 3;
 H123 = (1./(K - 2)).*(-sum(nu_eta.*F111,3));
 N = 3; K = 3;
 H133 = (1./(K - 2)).*((N - 2).*H111 - sum(nu_eta.*F121,3));
-N = 4; K = 3;
-H143 = (1./(K - 2)).*((N - 2).*H121 - sum(nu_eta.*F131,3));
-N = 5; K = 3;
-H153 = (1./(K - 2)).*((N - 2).*H131 - sum(nu_eta.*F141,3));
 
 K = 5;
 H125 = (1./(K - 2)).*(-sum(nu_eta.*F113,3));
@@ -435,36 +329,21 @@ N = 3; K = 5;
 H135 = (1./(K - 2)).*((N - 2).*H113 - sum(nu_eta.*F123,3));
 N = 4; K = 5;
 H145 = (1./(K - 2)).*((N - 2).*H123 - sum(nu_eta.*F133,3));
-N = 5; K = 5;
-H155 = (1./(K - 2)).*((N - 2).*H133 - sum(nu_eta.*F143,3));
 
 % 7.)
 K = 3;
 H213 = (1./(K - 2)).*(-sum(nu_xi.*F111,3));
 H223 = (1./(K - 2)).*(-sum(nu_xi.*F121,3));
-H233 = (1./(K - 2)).*(-sum(nu_xi.*F131,3));
-H243 = (1./(K - 2)).*(-sum(nu_xi.*F141,3));
 
 H215 = (1./(K - 2)).*(-sum(nu_xi.*F113,3));
 H225 = (1./(K - 2)).*(-sum(nu_xi.*F123,3));
 H235 = (1./(K - 2)).*(-sum(nu_xi.*F133,3));
-H245 = (1./(K - 2)).*(-sum(nu_xi.*F143,3));
 
 % 8.)
 H313 = -H133 - (h.^2).*H113 + H111;
-H413 = -H233 - (h.^2).*H213 + H211;
-H323 = -H143 - (h.^2).*H123 + H121;
-H333 = -H153 - (h.^2).*H133 + H131;
-H513 = -H333 - (h.^2).*H313 + H311;
-
 H315 = -H135 - (h.^2).*H115 + H113;
 H415 = -H235 - (h.^2).*H215 + H213;
 H325 = -H145 - (h.^2).*H125 + H123;
-H335 = -H155 - (h.^2).*H135 + H133;
-H515 = -H335 - (h.^2).*H315 + H313;
-
-% PAGE 127 / PAGE 187
-% out = eps(1,3,5,h);
 
 %%
 J11 = [reshape(3.*h.*H215,1,1,[]); reshape(3.*h.*H125,1,1,[]); reshape(H113 - 3.*(h.^2).*H115,1,1,[])];
@@ -487,9 +366,6 @@ infl_loc(:,6,:) = J11;
 infl_loc(:,:,idx_flp) = -infl_loc(:,:,idx_flp);
 
 infl_loc(1:2,:,idx_afx) = infl_loc(1:2,:,idx_afx).*reshape((h_orig./ztol),1,1,[]);
-
-% infl_loc(:,:,idx_on_element) = infl_loc(:,:,idx_on_element).*0;
-% disp('Turning off on element');
 
 end
 

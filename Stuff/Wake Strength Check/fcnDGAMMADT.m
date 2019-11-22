f64414fe7a41609ebfaf69663a0b3c281e080656
travis-k <tkrebs@ryerson.ca>
2019-11-22 10:37:53 -0500

    Added different FE timestep size option

--- Stuff/Wake Strength Check/fcnDGAMMADT.m
+++ Stuff/Wake Strength Check/fcnDGAMMADT.m
@@ -1,27 +1,30 @@
-function [CT_U, CL_U, matDGAMMADT] = fcnDGAMMADT(valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR)
+function [CT_U, CL_U, matDGAMMADT] = fcnDGAMMADT(skip, valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR)
 
-len = size(matLIFTFREE,1);
-for i = 1:len
-    if i <= len-2
-        matDGAMMADT(i,:) = (-matINTCIRC(i+2,:) + 4.*matINTCIRC(i+1,:) - 3.*matINTCIRC(i,:))./(2.*valDELTIME);
-    elseif i == len - 1
-        matDGAMMADT(i,:) = ((matINTCIRC(i+1,:) - matINTCIRC(i,:))./valDELTIME);
-    elseif i == len
-        matDGAMMADT(i,:) = ((matINTCIRC(i,:) - matINTCIRC(i-1,:))./valDELTIME);
-    end
-end
-
+% matDGAMMADT = nan(size(matLIFTFREE));
 % len = size(matLIFTFREE,1);
-% matDGAMMADT(1,:) = matINTCIRC(1,:).*0;
-% for i = 3:len
-%     if i <= len-2
-%         matDGAMMADT(i,:) = ((-matINTCIRC(i+2,:) + 8.*matINTCIRC(i+1,:) - 8.*matINTCIRC(i-1,:) + matINTCIRC(i-2,:))./(12.*valDELTIME));
-%     elseif i == len - 1
-%         matDGAMMADT(i,:) = ((matINTCIRC(i+1,:) - matINTCIRC(i-1,:))./(2.*valDELTIME));
-%     elseif i == len
-%         matDGAMMADT(i,:) = ((matINTCIRC(i,:) - matINTCIRC(i-1,:))./valDELTIME);
+% for i = 1:skip:len
+%     if i <= len-(2*skip)
+%         matDGAMMADT(i,:) = (-matINTCIRC(i+(2*skip),:) + 4.*matINTCIRC(i+skip,:) - 3.*matINTCIRC(i,:))./(2.*valDELTIME.*skip);
+%     elseif i > len-(2*skip) && i <= len - skip
+%         matDGAMMADT(i,:) = ((matINTCIRC(i+skip,:) - matINTCIRC(i,:))./(valDELTIME.*skip));
+%     else
+%         matDGAMMADT(i,:) = ((matINTCIRC(i,:) - matINTCIRC(i-skip,:))./(valDELTIME.*skip));
 %     end
 % end
+
+matDGAMMADT = nan(size(matLIFTFREE));
+len = size(matLIFTFREE,1);
+for i = 1:skip:len
+    if i <= skip
+        matDGAMMADT(i,:) = (-matINTCIRC(i+(2*skip),:) + 4.*matINTCIRC(i+skip,:) - 3.*matINTCIRC(i,:))./(2.*valDELTIME.*skip);
+    elseif i > skip && i <= len-(2*skip)
+        matDGAMMADT(i,:) = (-matINTCIRC(i+(2*skip),:) + 6.*matINTCIRC(i+skip,:) - 3.*matINTCIRC(i,:) - 2.*matINTCIRC(i-skip,:))./(6.*valDELTIME.*skip);
+    elseif i > len-(2*skip) && i <= len - skip
+        matDGAMMADT(i,:) = ((matINTCIRC(i+skip,:) - matINTCIRC(i,:))./(valDELTIME.*skip));
+    else
+        matDGAMMADT(i,:) = ((matINTCIRC(i,:) - matINTCIRC(i-skip,:))./(valDELTIME.*skip));
+    end
+end
 
 matDGAMMADT(1,:) = matDGAMMADT(1,:).*0;
 
@@ -30,7 +33,7 @@ tmpDVELIFT = tmpLIFTFREE + matLIFTIND;
 tmpDVEDRAG = matDRAGIND;
 tmpDVESIDE = matSIDEFREE + matSIDEIND;
 
-for i = 1:size(matLIFTFREE,1)
+for i = 1:skip:size(matLIFTFREE,1)
     tmpDVETHRUST(i,:) = dot(matDVELIFT_DIR(:,:,i).*tmpDVELIFT(i,:)', repmat([0 0 1], length(tmpDVELIFT(i,:)), 1), 2) ...
         + dot(matDVEDRAG_DIR(:,:,i).*tmpDVEDRAG(i,:)', repmat([0 0 1], length(tmpDVEDRAG(i,:)), 1), 2) ...
         + dot(matDVESIDE_DIR(:,:,i).*tmpDVESIDE(i,:)', repmat([0 0 1], length(tmpDVESIDE(i,:)), 1), 2);
@@ -44,7 +47,7 @@ for i = 1:size(matLIFTFREE,1)
 end
 
 CL_U = sum(tmpLIFTFREE,2)./(0.5.*valDENSITY.*valAREA.*(valUINF^2));
-
+CL_U = CL_U(~isnan(CL_U));
 
 end
 
--- Stuff/Wake Strength Check/kussner.m
+++ Stuff/Wake Strength Check/kussner.m
@@ -23,7 +23,7 @@ load('Kussner.mat')
 
 hFig22 = figure(22);
 clf(22);
-plot(((s+2)/2), c_l, '-k');
+plot(((s+2)/2), c_l, '-k', 'linewidth',2);
 box on
 axis tight
 grid minor
@@ -31,37 +31,46 @@ grid minor
 hold on
 % disp('THIS IS WITH APPARENT MASS!!!!!!!!!!')
 load('m5_dt1_wfix2.mat');
-[~, CL_U, ~] = fcnDGAMMADT(valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
+[~, CL_U, ~] = fcnDGAMMADT(1, valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
 s_t = [1:valMAXTIME].*valDELTIME;
 valAR = (valSPAN.^2)./valAREA;
 CL_U2D = CL_U.*((valAR + 2)/valAR);
 plot(s_t, CL_U2D, '-ok', 'LineWidth', 1);
+delt_time(1) = valDELTIME;
+ratio(1) = valDELTIME/(1/5);
 
-load('m5_dt0.5_wfix2.mat');
-[~, CL_U, ~] = fcnDGAMMADT(valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
-s_t = [1:valMAXTIME].*valDELTIME;
-valAR = (valSPAN.^2)./valAREA;
-CL_U2D = CL_U.*((valAR + 2)/valAR);
-plot(s_t, CL_U2D, '--bs', 'LineWidth', 1);
+% load('m5_dt0.5_wfix2.mat');
+% [~, CL_U, ~] = fcnDGAMMADT(1, valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
+% s_t = [1:valMAXTIME].*valDELTIME;
+% valAR = (valSPAN.^2)./valAREA;
+% CL_U2D = CL_U.*((valAR + 2)/valAR);
+% plot(s_t, CL_U2D, '--bs', 'LineWidth', 1);
 
-load('m5_dt0.25_wfix2.mat');
-[~, CL_U, ~] = fcnDGAMMADT(valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
+load('m5_dt0.2_wfix2.mat');
+[~, CL_U, ~] = fcnDGAMMADT(1, valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
 s_t = [1:valMAXTIME].*valDELTIME;
 valAR = (valSPAN.^2)./valAREA;
 CL_U2D = CL_U.*((valAR + 2)/valAR);
 plot(s_t, CL_U2D, '-.rd', 'LineWidth', 1);
+delt_time(2) = valDELTIME;
+ratio(2) = valDELTIME/(1/5);
 
-load('m5_dt0.2_wfix2.mat');
-[~, CL_U, ~] = fcnDGAMMADT(valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
-s_t = [1:valMAXTIME].*valDELTIME;
+load('m5_dt0.05_wfix2.mat');
+skip = 5;
+[~, CL_U, ~] = fcnDGAMMADT(skip, valDELTIME, strATYPE, matINTCIRC, valDENSITY, valRPM, valDIAM, valAREA, valUINF, matLIFTFREE, matLIFTIND, matDRAGIND, matSIDEFREE, matSIDEIND, matDVELIFT_DIR, matDVEDRAG_DIR, matDVESIDE_DIR);
+s_t = [1:skip:valMAXTIME].*valDELTIME;
 valAR = (valSPAN.^2)./valAREA;
 CL_U2D = CL_U.*((valAR + 2)/valAR);
 plot(s_t, CL_U2D, '--m^', 'LineWidth', 1);
+delt_time(3) = valDELTIME;
+ratio(3) = (valDELTIME*skip)/(1/5);
 
 hold off
 xlabel('Time (s)');
 ylabel('Lift Coefficient');
 
-legend('Kussner Function', ['\Delta_T = 1s, \Deltax_w/\Deltax_c = ', num2str(1/(1/5))], ['\Delta_T = 0.5s, \Deltax_w/\Deltax_c = ', num2str(0.5/(1/5))], ['\Delta_T = 0.25s, \Deltax_w/\Deltax_c = ', num2str(0.25/(1/5))], ['\Delta_T = 0.05s, \Deltax_w/\Deltax_c = ', num2str(0.05/(1/5))],'Location','SouthEast')
+legend('Kussner Function', ['\Delta_T = ', num2str(delt_time(1)), 's, \Deltax_w/\Deltax_c = ', num2str(ratio(1))], ...
+    ['\Delta_T = ', num2str(delt_time(2)), 's, \Deltax_w/\Deltax_c = ', num2str(ratio(2))], ...
+    ['\Delta_T = ', num2str(delt_time(3)), 's, \Deltax_w/\Deltax_c = ', num2str(ratio(3))],'Location','SouthEast')
 
 
